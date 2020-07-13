19328506d17755e34b2bcbefd85515a205c3ba41
hsuk <hsuk0409@gmail.com>
2020-07-13 21:45:30 +0900

    naver 로그인 및 스프링 시큐리티 적용하여 테스트 보완

--- src/main/java/com/springaws/study/springawssutdy/SpringAwsStudyApplication.java
+++ src/main/java/com/springaws/study/springawssutdy/SpringAwsStudyApplication.java
@@ -2,10 +2,8 @@ package com.springaws.study.springawssutdy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class SpringAwsStudyApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/springaws/study/springawssutdy/web/HelloControllerTest.java
+++ src/test/java/com/springaws/study/springawssutdy/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.springaws.study.springawssutdy.web;
 
+import com.springaws.study.springawssutdy.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,13 +16,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+                excludeFilters = {
+                    @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+                })
 public class HelloControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
+    @WithMockUser("USER")
     public void hello_반환한다() throws Exception {
         String hello = "hello";
         mockMvc.perform(get("/hello"))
@@ -27,6 +35,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser("USER")
     public void dto_반환한다() throws Exception {
         String name = "justin";
         int amount = 10000;

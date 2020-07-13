72742140a3712c81a18662292144726482cd1314
hsuk <hsuk0409@gmail.com>
2020-07-13 20:34:46 +0900

    반복되는 코드 어노테이션 기반으롭 변경

--- src/main/java/com/springaws/study/springawssutdy/web/IndexController.java
+++ src/main/java/com/springaws/study/springawssutdy/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.springaws.study.springawssutdy.web;
 
+import com.springaws.study.springawssutdy.config.auth.LoginUser;
 import com.springaws.study.springawssutdy.config.auth.dto.SessionUser;
 import com.springaws.study.springawssutdy.service.posts.PostsService;
 import com.springaws.study.springawssutdy.web.dto.PostsResponseDto;
@@ -9,19 +10,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) model.addAttribute("userName", user.getName());
         return "index";
